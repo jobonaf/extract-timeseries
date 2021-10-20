b137df86c9f678b2a1a38c415335b971f5f66e11
Giovanni Bonafè <midable@gmail.com>
2021-10-20 14:16:15 +0200

    some updating

--- R/prepare_packages.R
+++ R/prepare_packages.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("shiny","leaflet","leaflet.extras","glue","xts","dygraphs","zen4R",
-                      "raster","lubridate","dplyr","rgdal","futile.logger")
+list.of.packages <- c("shiny","leaflet","leaflet.extras","glue","xts","dygraphs",
+                      "raster","lubridate","dplyr","rgdal","futile.logger","zen4R")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
